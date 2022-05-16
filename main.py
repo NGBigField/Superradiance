dbad7befae9447997ce64990acda87f2fd137303
Nir Gutman <46134640+NGBigField@users.noreply.github.com>
2022-05-16 13:43:23 +0300

    comments

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+""" main
+
+#TODO:
+1. read equation 6.36
+
+2. d/dt  D^{+ cl}     6.49
+
+3. 6.45, 6.46 statistics
+
+4. Plot average intensity for many that_i values
+
+5. theta_i = (2/N)*alpha   P(theta_i^2)=(N/d)*exp{ -N*theta_i^2 / 4 }
+
+6. N Doesn't necessarily has to be huge
+
+"""
+
 # import example:
 from Evolution import main as EvolutionExample
 
