47a870d3bd30387ba3485980c036804a632886e2
Nir Gutman <NGBigfield@gmail.com>
2023-01-30 10:22:23 +0200

    renaming gkp sequence script

--- main_gkp_simple_sequence.py
+++ main_gkp_simple_sequence.py
@@ -19,7 +19,7 @@ from utils import (
 from fock import Fock
 
 # For coherent control
-from coherentcontrol import CoherentControl
+from coherentcontrol import CoherentControl, _DensityMatrixType
 
 # for plotting
 import matplotlib.pyplot as plt  # for plotting test results:
@@ -31,6 +31,32 @@ from optimization import learn_custom_operation, LearnedResults, minimize, learn
 #|                                helper functions                                    |#
 # ==================================================================================== #
 
+def _get_final_state(
+    ground_state:_DensityMatrixType, coherent_control:CoherentControl,
+    x1, x2
+):
+    # Basic pulses:
+    Sy = coherent_control.s_pulses.Sy
+    Sy2 = Sy@Sy
+    Sz = coherent_control.s_pulses.Sz
+
+    # Act with pulses:
+    rho = ground_state
+    rho = coherent_control.pulse_on_state(rho, x=x1, power=2) 
+    rho, z1 = learn_single_op(rho, Sz, Sy2)
+
+    z2 = pi/2
+
+    rho = coherent_control.pulse_on_state(rho, x=x2, power=1)
+    rho = coherent_control.pulse_on_state(rho, z=z2, power=2)
+
+    rho = coherent_control.pulse_on_state(rho, x=x2, power=1)
+    rho = coherent_control.pulse_on_state(rho, z=z2, power=2)
+
+    visuals.plot_light_wigner(rho)
+    visuals.draw_now()
+
+    return rho
 
 # ==================================================================================== #
 #|                                   main tests                                       |#
@@ -43,18 +69,15 @@ def _example_gkp2(
     # Define the basics:
     ground_state = Fock.ground_state_density_matrix(num_moments=num_moments)    
     coherent_control = CoherentControl(num_moments=num_moments)
-    Sx = coherent_control.s_pulses.Sx
-    Sy = coherent_control.s_pulses.Sy
-    Sy2 = Sy@Sy
-    Sz = coherent_control.s_pulses.Sz
+    final_state = lambda x1, x2 : _get_final_state(ground_state, coherent_control, x1, x2)
 
     # Derive size-specific variables:
     if num_moments==20:
         x1 = 0.02
         x2 = 0.8
     elif num_moments==40:
-        x1 = 0.042
-        x2 = 0.6
+        x1 = 0.09
+        x2 = 0.2
     elif num_moments==100:
         x1 = 0.02
         x2 = 0.4
@@ -62,24 +85,8 @@ def _example_gkp2(
         raise ValueError(f"This number is not supported. num_moments={num_moments}")
 
     # Act with Pulses:
-    rho = ground_state
-    rho = coherent_control.pulse_on_state(rho, x=x1, power=2) 
-    rho, z1 = learn_single_op(rho, Sz, Sy2)
-
-    z2 = pi/2
-
-    rho = coherent_control.pulse_on_state(rho, x=x2, power=1)
-    rho = coherent_control.pulse_on_state(rho, z=z2, power=2)
-
-    # rho = coherent_control.pulse_on_state(rho, x=z, power=1)
-
-    rho = coherent_control.pulse_on_state(rho, x=x2, power=1)
-    rho = coherent_control.pulse_on_state(rho, z=z2, power=2)
-
-    visuals.plot_light_wigner(rho)
-    visuals.draw_now()
-
-
+    x1, x2 = 0.072, 0.3
+    final_state(x1, x2)
     print("Finished.")
     
 
